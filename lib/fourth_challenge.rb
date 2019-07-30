d940e9b57303366c88063cb28f8b03f77d1743a0
Richard Lee <richlee81192@gmail.com>
2019-07-30 17:37:40 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,13 +4,13 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [{:name => "Steven",
-        :age => "30",
-        :attitude => "confused"},
+      :hero_friends => [{:name => "Benvolio",
+        :age => "17",
+        :attitude => "worried"},
         {
-        :name => "Nurse",
-        :age => "44",
-        :attitude => "worried"}
+        :name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"}
         }
       ]
    }, 
