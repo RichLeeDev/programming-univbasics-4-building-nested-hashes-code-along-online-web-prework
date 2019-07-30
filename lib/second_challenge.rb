133ceb940651d1d55fcf805cc0d8b6560f3c274a
Richard Lee <richlee81192@gmail.com>
2019-07-30 17:02:42 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,12 +3,15 @@ def second_challenge
     :montague => {
       patriarch: {}
       matriarch: {}
-      heroine: {}
-      heroine_friends: []
+      hero: {}
+      hero_friends: []
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      patriarch: {}
+      matriarch: {}
+      heroine: {}
+      heroine_friends: []
+      }
 
   
 end
