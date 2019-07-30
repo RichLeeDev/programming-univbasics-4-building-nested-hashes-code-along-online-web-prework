911191d3ba8e0ed1bad89b5b198c01d8caaac538
Richard Lee <richlee81192@gmail.com>
2019-07-30 17:42:53 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -14,7 +14,7 @@ def fourth_challenge
         }
       
    }, 
-   :capulet = {
+   :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
